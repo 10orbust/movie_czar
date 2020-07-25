c416763ac1d2b695dd24f3decf735e4d1fa52ef8
Patrick McKernin <patrick@firstdraft.com>
2020-07-25 14:06:57 -0500

    Wrote seeds file and made one change to User migration

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_07_12_185748) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "firstname"
+    t.string "first_name"
     t.string "last_name"
     t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
